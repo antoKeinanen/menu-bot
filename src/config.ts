720d767c393c950cb11a32af86c5151cf16d9395
antoKeinanen <anto.keinanen@gmail.com>
2024-03-31 15:16:38 +0300

    re enable all menus

--- src/config.ts
+++ src/config.ts
@@ -13,18 +13,6 @@ export const tagEmojis: Record<string, string> = {
   veg: "<:vegaaninen:1223943984649470042>",
 };
 
-/*
-PORTAALI_WEBHOOK=""
-KAISA_WEBHOOK=""
-KAIVOPIHA_WEBHOOK=""
-METSATALO_WEBHOOK=""
-OLIVIA_WEBHOOK=""
-PORTHANIA_WEBHOOK=""
-ROTUNDA_WEBHOOK=""
-SOCKOM_WEBHOOK=""
-TOPELIAS_WEBHOOK=""
-*/
-
 export const config: IConfig = {
   restaurants: [
     // Kumpula
@@ -33,95 +21,95 @@ export const config: IConfig = {
       endpoint: "https://www.lounaat.info/lounas/unicafe-chemicum/helsinki",
       parseMode: ParseMode.LOUNAAT_INFO,
     },
-    // {
-    //   name: "Exactum",
-    //   endpoint: "https://www.lounaat.info/lounas/unicafe-exactum/helsinki",
-    //   parseMode: ParseMode.LOUNAAT_INFO,
-    // },
-    // {
-    //   name: "Physicum",
-    //   endpoint: "https://www.lounaat.info/lounas/unicafe-physicum/helsinki",
-    //   parseMode: ParseMode.LOUNAAT_INFO,
-    // },
+    {
+      name: "Exactum",
+      endpoint: "https://www.lounaat.info/lounas/unicafe-exactum/helsinki",
+      parseMode: ParseMode.LOUNAAT_INFO,
+    },
+    {
+      name: "Physicum",
+      endpoint: "https://www.lounaat.info/lounas/unicafe-physicum/helsinki",
+      parseMode: ParseMode.LOUNAAT_INFO,
+    },
 
-    // // Helsingin keskusta
-    // {
-    //   name: "Portaali",
-    //   endpoint:
-    //     "https://www.lounaat.info/lounas/unicafe-cafe-portaali/helsinki",
-    //   parseMode: ParseMode.LOUNAAT_INFO,
-    // },
-    // {
-    //   name: "Kaisa",
-    //   endpoint:
-    //     "https://www.lounaat.info/lounas/unicafe-well-kaisa-talo/helsinki",
-    //   parseMode: ParseMode.LOUNAAT_INFO,
-    // },
-    // {
-    //   name: "Kaivopiha",
-    //   endpoint:
-    //     "https://www.lounaat.info/lounas/unicafe-ylioppilasaukio/helsinki",
-    //   parseMode: ParseMode.LOUNAAT_INFO,
-    // },
-    // {
-    //   name: "Metsatalo",
-    //   endpoint: "https://www.lounaat.info/lounas/unicafe-metsatalo/helsinki",
-    //   parseMode: ParseMode.LOUNAAT_INFO,
-    // },
-    // {
-    //   name: "Olivia",
-    //   endpoint: "https://www.lounaat.info/lounas/unicafe-olivia/helsinki",
-    //   parseMode: ParseMode.LOUNAAT_INFO,
-    // },
-    // {
-    //   name: "Porthania",
-    //   endpoint: "https://www.lounaat.info/lounas/unicafe-porthania/helsinki",
-    //   parseMode: ParseMode.LOUNAAT_INFO,
-    // },
-    // {
-    //   name: "Rotunda",
-    //   endpoint: "https://www.lounaat.info/lounas/unicafe-rotunda/helsinki",
-    //   parseMode: ParseMode.LOUNAAT_INFO,
-    // },
-    // {
-    //   name: "Sockom",
-    //   endpoint: "https://www.lounaat.info/lounas/unicafe-soc-kom/helsinki",
-    //   parseMode: ParseMode.LOUNAAT_INFO,
-    // },
-    // {
-    //   name: "Topelias",
-    //   endpoint:
-    //     "https://www.lounaat.info/lounas/unicafe-pesco-vege-topelias/helsinki",
-    //   parseMode: ParseMode.LOUNAAT_INFO,
-    // },
+    // Helsingin keskusta
+    {
+      name: "Portaali",
+      endpoint:
+        "https://www.lounaat.info/lounas/unicafe-cafe-portaali/helsinki",
+      parseMode: ParseMode.LOUNAAT_INFO,
+    },
+    {
+      name: "Kaisa",
+      endpoint:
+        "https://www.lounaat.info/lounas/unicafe-well-kaisa-talo/helsinki",
+      parseMode: ParseMode.LOUNAAT_INFO,
+    },
+    {
+      name: "Kaivopiha",
+      endpoint:
+        "https://www.lounaat.info/lounas/unicafe-ylioppilasaukio/helsinki",
+      parseMode: ParseMode.LOUNAAT_INFO,
+    },
+    {
+      name: "Metsatalo",
+      endpoint: "https://www.lounaat.info/lounas/unicafe-metsatalo/helsinki",
+      parseMode: ParseMode.LOUNAAT_INFO,
+    },
+    {
+      name: "Olivia",
+      endpoint: "https://www.lounaat.info/lounas/unicafe-olivia/helsinki",
+      parseMode: ParseMode.LOUNAAT_INFO,
+    },
+    {
+      name: "Porthania",
+      endpoint: "https://www.lounaat.info/lounas/unicafe-porthania/helsinki",
+      parseMode: ParseMode.LOUNAAT_INFO,
+    },
+    {
+      name: "Rotunda",
+      endpoint: "https://www.lounaat.info/lounas/unicafe-rotunda/helsinki",
+      parseMode: ParseMode.LOUNAAT_INFO,
+    },
+    {
+      name: "Sockom",
+      endpoint: "https://www.lounaat.info/lounas/unicafe-soc-kom/helsinki",
+      parseMode: ParseMode.LOUNAAT_INFO,
+    },
+    {
+      name: "Topelias",
+      endpoint:
+        "https://www.lounaat.info/lounas/unicafe-pesco-vege-topelias/helsinki",
+      parseMode: ParseMode.LOUNAAT_INFO,
+    },
 
-    // // Meilahti
-    // {
-    //   name: "Meilahti",
-    //   endpoint: "https://www.lounaat.info/lounas/unicafe-meilahti/helsinki",
-    //   parseMode: ParseMode.LOUNAAT_INFO,
-    // },
+    // Meilahti
+    {
+      name: "Meilahti",
+      endpoint: "https://www.lounaat.info/lounas/unicafe-meilahti/helsinki",
+      parseMode: ParseMode.LOUNAAT_INFO,
+    },
 
-    // // Viikki
-    // {
-    //   name: "Biokeskus3",
-    //   endpoint: "https://www.lounaat.info/lounas/unicafe-biokeskus/helsinki",
-    //   parseMode: ParseMode.LOUNAAT_INFO,
-    // },
-    // {
-    //   name: "Infokeskus",
-    //   endpoint: "https://www.lounaat.info/lounas/unicafe-infokeskus/helsinki",
-    //   parseMode: ParseMode.LOUNAAT_INFO,
-    // },
-    // {
-    //   name: "Tahka",
-    //   endpoint: "https://www.lounaat.info/lounas/unicafe-tahka/helsinki",
-    //   parseMode: ParseMode.LOUNAAT_INFO,
-    // },
-    // {
-    //   name: "Viikuna",
-    //   endpoint: "https://www.lounaat.info/lounas/unicafe-viikuna/helsinki",
-    //   parseMode: ParseMode.LOUNAAT_INFO,
-    // },
+    // Viikki
+    {
+      name: "Biokeskus3",
+      endpoint: "https://www.lounaat.info/lounas/unicafe-biokeskus/helsinki",
+      parseMode: ParseMode.LOUNAAT_INFO,
+    },
+    {
+      name: "Infokeskus",
+      endpoint: "https://www.lounaat.info/lounas/unicafe-infokeskus/helsinki",
+      parseMode: ParseMode.LOUNAAT_INFO,
+    },
+    {
+      name: "Tahka",
+      endpoint: "https://www.lounaat.info/lounas/unicafe-tahka/helsinki",
+      parseMode: ParseMode.LOUNAAT_INFO,
+    },
+    {
+      name: "Viikuna",
+      endpoint: "https://www.lounaat.info/lounas/unicafe-viikuna/helsinki",
+      parseMode: ParseMode.LOUNAAT_INFO,
+    },
   ],
 };
